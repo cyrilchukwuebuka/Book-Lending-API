90d57642bad7189b0454ae6afeee79959d833fd1
ThatSameBlackBoyFromAfrica <muofunanya3@gmail.com>
2021-10-25 00:14:09 +0100

    initial commit, created my server and route handlers with data directory

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Book-Lending-API
+
+An Api that allows a registered user to borrow book  
+from the library using the user issued ID
+
+### Technologies used
+* Raw Http (used to create and get the server up and running)
+
+### Functionalities
+* Book Route for:
+ * Creating and Adding new book to the library when the url(/books) is visited with a POST method
+ * Updating already existing book when (/books?<params>) is visited with PUT method
+ * Reading(fetching) a particular book from the library when (/book) is visited with the GET method
+ * Deleting a particular book from the library when (/books?<params>) is visited with the DELETE method
--- index.js
+++ index.js
@@ -2,72 +2,103 @@
  * Entry file to out application
  */
 
-const http = require('http');
-const url = require('url');
-const { StringDecoder } = require('string_decoder');
-const routeHandler = require('./lib/routehandler');
+const http = require("http");
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
+const routeHandler = require("./lib/routehandler");
 
 const httpServer = http.createServer((req, res) => {
-    //parse the incoming url
-    const parsedurl = url.parse(req.url, true);
-    //get the path name
-    const pathname = parsedurl.pathname;
-    const trimedPath = pathname.replace(/^\/+|\/+$/g, "");
-    //get the Http Method
-    const method = req.method.toLowerCase();
-    //get the query string
-    const queryStringObj = parsedurl.query;
-    //get the request headers
-    const headers = req.headers;
+    console.log(req)
+    console.log(`
+    `)
+    console.log(res)
+  //parse the incoming url
+  const parsedurl = url.parse(req.url, true);
+  //get the path name
+  const pathname = parsedurl.pathname;
+  console.log(pathname)
+  const trimedPath = pathname.replace(/^\/+|\/+$/g, "");
+  console.log(trimedPath)
+  //get the Http Method
+  const method = req.method.toLowerCase();
+  //get the query string
+  const queryStringObj = parsedurl.query;
+  //get the request headers
+  const headers = req.headers;
 
-    const decoder = new StringDecoder('utf-8');
-    var buffer = '';
-    req.on('data', (data) => {
-        buffer += decoder.write(data);
+  const decoder = new StringDecoder("utf-8");
+  var buffer = "";
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
+
+  req.on("end", () => {
+    buffer += decoder.end();
+
+    const parsedPayload = buffer !== "" ? JSON.parse(buffer) : {};
+
+    const data = {
+      trimedPath: trimedPath,
+      query: queryStringObj,
+      method: method,
+      headers: headers,
+      payload: parsedPayload,
+    };
+
+    const chosenHandler =
+      typeof router[trimedPath] !== "undefined"
+        ? router[trimedPath]
+        : router.notfound;
+    //use the chosen handler to handle the request
+    chosenHandler(data, (statusCode, result) => {
+      statusCode = typeof statusCode === "number" ? statusCode : 200;
+      result = typeof res === "object" ? result : {};
+
+      const responseObj = JSON.stringify(result);
+
+      res.setHeader("Content-type", "application/json");
+      res.writeHead(statusCode);
+
+      res.write(responseObj);
+      res.end();
+
+      console.log(
+        `the url visited was, ${trimedPath} and the method is ${method}`
+      );
     });
-
-    req.on('end', () => {
-        buffer += decoder.end();
-
-        const parsedPayload = JSON.parse(buffer);
-
-        const data = {
-            trimedPath: trimedPath,
-            query: queryStringObj,
-            method: method,
-            headers: headers,
-            payload: parsedPayload
-        };
-
-        const chosenHandler = typeof (router[trimedPath]) !== 'undefined' ? router[trimedPath] : router.notfound;
-        //use the chosen handler to handle the request
-        chosenHandler(data, (statusCode, result) => {
-
-            statusCode = typeof (statusCode) === 'number' ? statusCode : 200;
-            result = typeof (res) === 'object' ? result : {};
-
-            const responseObj = JSON.stringify(result);
-
-            res.setHeader('Content-type', "application/json");
-            res.writeHead(statusCode);
-
-            res.write(responseObj);
-            res.end();
-
-            console.log(`the url visited was, ${trimedPath} and the method is ${method}`);
-        });
-    });
+  });
 });
 
 //start listening on port 8080
 httpServer.listen(8080, () => {
-    console.log("server is listening on port 8080");
+  console.log("server is listening on port 8080");
 });
 
 const router = {
-    ping : routeHandler.ping,
-    books : routeHandler.Books,
-    notfound : routeHandler.notfound
-}
+  ping: routeHandler.ping,
+  books: routeHandler.Books,
+  user: routeHandler.User,
+  borrow: routeHandler.Borrow,
+  notfound: routeHandler.notfound,
+};
 
 
+/**
+ * Admin Function
+ * Registering a user will require us creating another file
+ * where we store the users we registered with there names and password
+ * and the books collected from the library
+ *
+ * User Function
+ * Requests for a book from the BOOKSHELF
+ *
+ *
+ *
+ * create three routes
+ * 1. for creating a user  url = /createuser
+ * 2. for user to borrow a book   url = /borrowbook
+ * 3. for user to return a book    url = /returnbook
+ * 
+ * 
+ * Returning a borrowed book should use a post request
+ */
